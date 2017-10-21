e0ad7bce8c05d91429238d183f2f47a56af68518
admin <dangtinhtong@gmail.com>
2017-10-21 23:04:16 +0700

    First commit

--- Rectangle/main.m
+++ Rectangle/main.m
@@ -7,11 +7,54 @@
 //
 
 #import <Foundation/Foundation.h>
+// Interface section
+@interface Rectangle : NSObject
+{
+    int width;
+    int heigh;
+}
+-(void) setWidth: (int) a;
+-(void) setHeigh: (int) b;
+-(int) width;
+-(int) heigh;
+-(int) Area;
+-(int) Perimeter;
 
+@end
+
+
+// Implementation section
+@implementation Rectangle
+-(void) setWidth:(int)a{
+    width =a;
+}
+-(void) setHeigh:(int)b{
+    heigh =b;
+}
+-(int) width {
+    return width;
+}
+-(int) heigh{
+    return heigh;
+}
+-(int ) Area {
+    return  width*heigh;
+}
+-(int) Perimeter {
+    return (width + heigh)*2;
+}
+
+@end
+
+// Main section
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+    
+    Rectangle *myRectangle = [[Rectangle alloc] init];
+    [myRectangle setWidth:5];
+    [myRectangle setHeigh:10];
+    NSLog(@"The area is %d", [myRectangle Area]);
+    NSLog(@"The perimetter is %d",[myRectangle Perimeter]);
+    
+   
     return 0;
 }
