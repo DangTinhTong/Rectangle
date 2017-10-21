112711187b739f8305486c90308b8d4f030fe45d
admin <dangtinhtong@gmail.com>
2017-10-21 22:53:45 +0700

    Initial Commit

---
+++ Rectangle/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Rectangle
+//
+//  Created by admin on 10/21/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
